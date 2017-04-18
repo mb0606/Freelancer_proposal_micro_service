e866c745d3448089bde2618f6d059b04e4260868
Marco Berardini <mberardini@evolution-man.com>
2017-04-18 13:48:09 -0700

    [Server] Added Proposal Model.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |proposal|
+Proposal.create!(
+  customer: "Customer #{proposal}",
+  portfolio_url: 'http://google.com',
+  tools: "Ruby on Rails, Angular 2, Postgres",
+  estimated_hours: (80 + proposal),
+  hourly_rate: 120,
+  weeks_to_complete: 12,
+  client_email: "me@exapmle#{proposal}.com",
+)
+end
+
